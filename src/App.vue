70070b2cdc75005c7b9a9452a5421485709a3a34
sakkurthisashank <sakkurthisashank@gmail.com>
2023-01-17 20:02:59 -0800

    chore: initialize firebase and tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+import "./styles/style.css";
+
+</script>
+
+<template></template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
+import { VueFire, VueFireAuth } from "vuefire";
 import App from "./App.vue";
+import { firebase } from "./db/config";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(VueFire, { firebase, modules: [VueFireAuth()] });
+
+app.mount("#app");
